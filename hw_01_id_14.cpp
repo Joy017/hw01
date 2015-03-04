15add702b7e87eef9160e1270ec6076ae3933e16
racsoyzil <osckarok@yahoo.com>
2015-03-03 19:44:31 -0600

    tarea 01
    declaramos tres variables de diferente tipo y calculamos su promedio

--- hw_01_id_14.cpp
+++ hw_01_id_14.cpp
@@ -1,19 +1,15 @@
-// Author:
-// Date:
-// Version:
-
-/*
-Homework DD
-this assignment  ...
-*/
-
-#include <iostream>
-
-int main ()
-{
-  // TODO your code here!!!!
-
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+//Autor:Oscar GArcia Avila
+//Date: 2/22/2015
+//Version:01
+//Homework 01 implement three variables and print the average
+//en esta tarea se declararan tres tipos diferentes de variables y se calculara el promedio 
+#include<iostream>//biblioteca
+int main(){
+	int x=2;//declaramos un tipo de variable
+	float y=3.9;//declaramos otro tipo de variable con decimales
+	double z=6.9;//declaramos otro tipo de variable doble
+	double p=(x+y+z)/3;//hacemos la operacion en una variable doble para calcular el promedio
+	std::cout<<"el promedio de los tres diferentes valores es:\n"<<p<<std::endl;//imprimimos el promedio
+	return 0;//valor de retorno
+	
 }
