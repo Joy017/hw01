0213b9b4b4216f730c168deccd50f34efe7fbad5
racsoyzil <osckarok@yahoo.com>
2015-03-10 21:23:32 -0600

    homework 01
    tres valores para cada tipo de dato y a esos tres valores de esos tres
    tipos de datos se les saca el promedio

--- hw_01_id_14.cpp
+++ hw_01_id_14.cpp
@@ -5,11 +5,18 @@
 //en esta tarea se declararan tres tipos diferentes de variables y se calculara el promedio 
 #include<iostream>//biblioteca
 int main(){
-	int x=2;//declaramos un tipo de variable
-	float y=3.9;//declaramos otro tipo de variable con decimales
-	double z=6.9;//declaramos otro tipo de variable doble
-	double p=(x+y+z)/3;//hacemos la operacion en una variable doble para calcular el promedio
-	std::cout<<"el promedio de los tres diferentes valores es:\n"<<p<<std::endl;//imprimimos el promedio
+	int r=2,s=4,t=6 ;//declaramos un tipo de variable
+	float x=3.9,y=4.3,z=2.3;//declaramos otro tipo de variable con decimales
+	double o=6.9,p=32,q=56;//declaramos otro tipo de variable doble
+	double a=(r+s+t)/3;//hacemos la operacion en una variable doble para calcular el promedio
+    double b=(x+y+z)/3;
+    double c=(o+p+q)/3;
+    std::cout<<"los numero enteros son: "<<r<< " , " <<s <<" , " <<t<<" , " <<std::endl;
+    std::cout<<"los numero float son: "<<x<<" , " << y<< " , " <<z<<" , "<<std::endl;
+    std::cout<<"los numero double son: "<<o<<" , " << p<< " , " <<q<<" , "<<std::endl;
+	std::cout<<"el promedio de los tres valores enteros es:\n"<<a<<std::endl;//imprimimos el promedio de cada tipo de dato
+    std::cout<<"el promedio de los tres valores float es:\n"<<b<<std::endl;
+	std::cout<<"el promedio de los tres valores double es:\n"<<c<<std::endl;
 	return 0;//valor de retorno
 	
 }
